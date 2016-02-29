c969ba22ffea3f6cf0b0f53863d55f29443040da
mountainpenguin <pinguino.de.montana@googlemail.com>
2016-02-29 11:11:42 +0000

    Allow manual assignment of poles if can't retrieve automagically

--- lineage/lineage_lib/poles.py
+++ lineage/lineage_lib/poles.py
@@ -3,8 +3,96 @@
 import numpy as np
 import json
 import os
+import matplotlib.pyplot as plt
+import matplotlib.patches
+matplotlib.rcParams["text.usetex"] = True
+import glob
+import scipy.misc
 
 from lineage_lib import track
+
+
+
+class AxBase(object):
+    def __init__(self, ax):
+        self.ax = ax
+        self._postinit()
+
+    def _postinit(self):
+        pass
+
+    def __getattr__(self, attr):
+        return getattr(self.ax, attr)
+
+class NoAx(AxBase):
+    def _postinit(self):
+        self.ax.axis("off")
+
+
+class ManualAssign(object):
+    def __init__(self, c0, c1):
+        self.assignment = None
+        self.phase_files = sorted(glob.glob("B/*.tif"))
+        self.fig = plt.figure()
+        self.fig.canvas.mpl_connect(
+            "pick_event", self.pick_event
+        )
+        self.fig.canvas.mpl_connect(
+            "key_press_event", self.key_press
+        )
+
+        self.ax0 = NoAx(self.fig.add_subplot(121))
+        self.ax1 = NoAx(self.fig.add_subplot(
+            122, sharex=self.ax0, sharey=self.ax0
+        ))
+        self.ax1.set_title(r"Select \textbf{NEW} pole")
+
+        self.plot_c1(c1)
+        self.plot_c0(c0, c1)
+
+    def get_assignment(self):
+        plt.tight_layout()
+        plt.show()
+        return self.assignment
+
+    def plot_mesh(self, c, ax):
+        ax.plot(c.mesh[:, 0], c.mesh[:, 1], "y")
+        ax.plot(c.mesh[:, 2], c.mesh[:, 3], "y")
+
+    def pick_event(self, e):
+        if self.assignment is not None:
+            # reset
+            self.handle0.set_color("b")
+            self.handle1.set_color("b")
+        e.artist.set_color("r")
+        self.assignment = e.artist.pole_assignment
+        plt.draw()
+
+    def key_press(self, e):
+        if e.key == "enter":
+            plt.close()
+
+    def plot_c0(self, c0, c1):
+        if c0:
+            i0 = scipy.misc.imread(self.phase_files[c0.frame - 1])
+            self.plot_mesh(c0, self.ax0)
+        else:
+            i1 = scipy.misc.imread(self.phase_files[c1.frame - 1])
+            i0 = np.zeros(i1.shape)
+        self.ax0.imshow(i0, cmap=plt.cm.gray)
+
+    def plot_c1(self, c1):
+        i1 = scipy.misc.imread(self.phase_files[c1.frame - 1])
+        self.ax1.imshow(i1, cmap=plt.cm.gray)
+
+        p0 = c1.mesh[0, 0:2]
+        p1 = c1.mesh[-1, 0:2]
+        self.handle0 = matplotlib.patches.Circle(p0, picker=True)
+        self.handle0.pole_assignment = 0
+        self.handle1 = matplotlib.patches.Circle(p1, picker=True)
+        self.handle1.pole_assignment = -1
+        self.ax1.add_artist(self.handle0)
+        self.ax1.add_artist(self.handle1)
 
 
 class PoleAssign(object):
@@ -64,6 +152,11 @@ class PoleAssign(object):
         else:
             return None
 
+    def manual_assignment(self, c0, c1):
+        M = ManualAssign(c0, c1)
+        assignment = M.get_assignment()
+        return assignment
+
     def assign_poles(self, force=False):
         if os.path.exists("poles.json") and not force:
             return json.loads(open("poles.json").read())
@@ -75,6 +168,12 @@ class PoleAssign(object):
             # assign a pole to the first member of the lineage only
             # orientation parameter will cover rest in spot_analysis routines
             pole_assignment = self.assign_new_pole(progenitor.parent, progenitor.id)
+            if not pole_assignment:
+                pole_assigment = self.manual_assignment(
+                    self.frames.cell(progenitor.parent),  # parent      (n - 1)
+                    progenitor,  # this cell                            (n)
+                )
+
             assignments[progenitor.id] = pole_assignment
             while type(progenitor.children) is str:
                 progenitor = self.frames.cell(progenitor.children)
